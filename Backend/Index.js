9dbda061e59452fac04f9bd171a234249603e691
Utpal Patel <utpalpatel85116@gmail.com>
2025-03-05 15:16:55 +0530

    connected to database

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import mongoose from "mongoose";
 dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to Mongo");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -11,4 +19,6 @@ app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+  console.log("env<><>",process.env.LLL);
+  
 });
